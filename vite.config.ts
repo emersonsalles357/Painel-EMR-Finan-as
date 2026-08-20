facb9b7c82d7c1f2d2550b61b5c13ec998105f85
emersonsalles357 <emerson.vieirasalles2002@gmail.com>
2026-08-20 19:45:08 -0300

    Configura base path no vite.config.ts para o GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Painel-EMR-Finan-as/",
   plugins: [react()],
+  base: "/Painel-EMR-Finan-as/", // <-- Adicione esta linha exata
 });
